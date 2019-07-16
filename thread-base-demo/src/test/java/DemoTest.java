cab14824947a2c5fc031a8be3edd861b639d650a
huayang.bai <1053091115@qq.com>
2019-07-16 09:05:11 +0800

    线程学习demo

--- thread-base-demo/src/test/java/DemoTest.java
+++ thread-base-demo/src/test/java/DemoTest.java
@@ -7,12 +7,19 @@
  */
 public class DemoTest {
 
+
     public static void main(String[] args) {
-        // 这行代码就会在开辟20m的内存空间
-        byte[] b = new byte[10 * 1024 * 1024];
-
-        System.out.println((0x00000000ffb80000-0x00000000ff980000)/1024);
-
+        // 获取当前jvm的堆内存的剩余内存大小
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        // 获取当前jvm的堆内存的总内存大小
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        // 获取当前jvm的堆内存的最大内存大小
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        // 创建一个对象，在堆内存中开辟一个块大小为4M的内存空间，存放这个对象
+        byte[] b = new byte[4 * 1024 * 1024];
+        System.out.println("当前堆内存剩余空间大小：" + freeMemory);
+        System.out.println("当前堆内存的总内存大小：" + totalMemory);
+        System.out.println("当前堆内存最大内存大小：" + maxMemory);
     }
 
 }
