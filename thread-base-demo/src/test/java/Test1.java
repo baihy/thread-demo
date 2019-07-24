593df6ecd0bafa872be0a936e89285cc50bcddb2
huayang.bai <1053091115@qq.com>
2019-07-24 16:08:11 +0800

    添加了测试老年代和新生代的值

---
+++ thread-base-demo/src/test/java/Test1.java
@@ -0,0 +1,19 @@
+/**
+ * @projectName: thread-demo
+ * @packageName: PACKAGE_NAME
+ * @description:
+ * @author: huayang.bai
+ * @date: 2019/07/24 16:06
+ */
+public class Test1 {
+
+    public static void main(String[] args){
+        try {
+            // do something
+            System.exit(0);
+        } finally{
+            System.out.println("***************");
+        }
+    }
+
+}
