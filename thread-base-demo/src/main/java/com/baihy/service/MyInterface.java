127579a1e64fe1bb6a425aa93d00c0a0ef1b42df
huayang.bai <1053091115@qq.com>
2019-07-31 10:42:37 +0800

    添加了测试老年代和新生代的值

---
+++ thread-base-demo/src/main/java/com/baihy/service/MyInterface.java
@@ -0,0 +1,11 @@
+package com.baihy.service;
+
+/**
+ * @projectName: thread-demo
+ * @packageName: com.baihy.service
+ * @description:
+ * @author: huayang.bai
+ * @date: 2019/07/31 9:20
+ */
+public interface  MyInterface {
+}
